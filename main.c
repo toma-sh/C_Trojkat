bc31311edf0905635bf0438bb27654b3a521e4f2
toma-sh <zoltom83@gmail.com>
2023-06-14 19:46:51 +0200

    creating a project, adding functions

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int rysuj_pros(int n);
+int rysuj_row(int n);
+
+int main(void)
+{
+	
+	return 0;
+}
+
+int rysuj_pros(int n)
+{
+	
+	return 0;
+}
+
+int rysuj_row(int n)
+{
+	
+	return 0;
+}
